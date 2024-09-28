ab78428e76a40dc777ce874dbed7ef27b3523ce3
Juan José Osorno <166628503+JUANJO1325@users.noreply.github.com>
2024-09-28 09:33:12 -0500

    De esta manera visualizamos 1 fecha en especifico

--- src/import.py
+++ src/import.py
@@ -1,7 +1,8 @@
 import pandas as pd 
 import openpyxl
 from tabulate import tabulate
+columnas=[0,3]
 
-
-dt=pd.read_excel("ejemplo.xlsx", sheet_name="Hoja1", header=None)
-print(tabulate(dt
+dt=pd.read_excel("ejemplo.xlsx", sheet_name="Hoja1", header=0, usecols=columnas)
+print(tabulate(dt))
+print(dt[dt["28 de julio"]==0])
